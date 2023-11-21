5089737e2deba88ececab414444224962e1b83c1
Skrylleur <106743803+Skrylleur@users.noreply.github.com>
2023-11-21 16:43:52 +0100

    new

--- docs/assets/ex-js/exercices.js
+++ docs/assets/ex-js/exercices.js
@@ -1,18 +1,18 @@
 //Exercice 1
-var ancienneCouleur
+let ancienneCouleur
 
 function ChangeColor () {
-    var body = document.body;
+    let body = document.body;
     body.style.background = "#94D1BE"; 
 }
 
 function originalColor () {
-    var body = document.body;
+    let body = document.body;
     body.style.background = ancienneCouleur;
 }
 
 function changeColor2 () {
-    var body = document.body;
+    let body = document.body;
 
     if(body.style.background === "rgb(255, 255, 255)") {
         body.style.background = "#94D1BE";
@@ -23,11 +23,11 @@ function changeColor2 () {
 
 //Exercice 2
 function AddText() {
-    var NewElement = document.getElementById('nom').value;
+    let NewElement = document.getElementById('nom').value;
 
     if (NewElement !== "") {
-        var liste = document.getElementById('MyList');
-        var NewItem = document.createElement('li');
+        let liste = document.getElementById('MyList');
+        let NewItem = document.createElement('li');
         NewItem.textContent = NewElement;
         liste.appendChild(NewItem);
 
@@ -37,12 +37,12 @@ function AddText() {
 
 //Exercice 3
 function Addition() {
-    var Value1 = document.getElementById('nb1').value;
-    var Value2 = document.getElementById('nb2').value;
+    let Value1 = document.getElementById('nb1').value;
+    let Value2 = document.getElementById('nb2').value;
 
     if (Value1 !== '' && Value2 !== '') {
-        var response = document.getElementById('Response');
-        var result = document.createElement('p');
+        let response = document.getElementById('Response');
+        let result = document.createElement('p');
         result.textContent = parseInt(Value1) + parseInt(Value2);
         response.innerHTML = '';
         response.appendChild(result);
@@ -53,13 +53,61 @@ function Addition() {
 }
 
 //Exercice 4
-document.getElementById('imageSelector').addEventListener('change', function() {
-    var selectedValue = this.value;
-    var imageDisplay = document.getElementById('imageDisplay');
+document.getElementById('imageSelector').addEventListener('change', function(event) {
+    let selectedValue = this.value;
+    let imageDisplay = document.getElementById('imageDisplay');
     imageDisplay.src = selectedValue;
+    consolelog(imageDisplay);
 });
 
 //Exercice 5 
 function afficherAlert() {
     alert('Touche pas il est mineur')
 }
+
+//Exercice 6
+/*function Horloge()  {
+   let date = new Date();
+   let hours = date.getHours();
+   let minutes = date.getMinutes();
+   let seconds = date.getSeconds();
+   hours = hours < 10 ? '0' + hours : hours;
+   minutes = minutes < 10 ? '0' + minutes : minutes;
+   seconds = seconds < 10 ? '0' + seconds : seconds;
+
+   document.Clock.horloge.value = hours;
+
+   //let timeString = hours + ":" + minutes + ":" + seconds;
+
+   document.getElementById(horloge).innerText = timeString;
+
+   timerID = setTimeout("showtime()",1000);
+   timerActif = true;
+
+}*/
+let timerID = null;
+let timerActif = false;
+
+function stopClock() {
+  if (timerActif) clearTimeout(timerID);
+  timerActif = false;
+}
+
+function startClock() {
+  stopClock();
+  showtime();
+}
+function showtime() {
+    let now = new Date();
+    let hour = now.getHours();
+    let min = now.getMinutes();
+    let sec = now.getSeconds();
+    heure = (hour > 9? hour:"0" + hour);
+    heure += ":" + (min > 9? min:"0" + min);
+    heure += ":" + (sec > 9? sec:"0" + sec);
+    document.Clock.Horloge.value = heure;
+    //pour mettre l'horloge dans la barre de status :
+    //window.status = heure;
+    timerID = setTimeout("showtime()",1000);
+    timerActif = true;
+  }
